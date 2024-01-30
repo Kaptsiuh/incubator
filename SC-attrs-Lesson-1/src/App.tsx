b98d5a94833a707f6232c05d1d07b8b5b8c7f416
Dima Berry <dimaberry1@gmail.com>
2024-01-30 20:18:52 +0100

    feat: solve ferst lesson

--- SC-attrs-Lesson-1/src/App.tsx
+++ SC-attrs-Lesson-1/src/App.tsx
@@ -1,30 +1,35 @@
-import './App.css';
-import styled from 'styled-components';
-
+import "./App.css";
+import styled from "styled-components";
 
 function App() {
-    return (
-        <div className="App">
-            <Title>Styled-components <span>.attrs</span> method</Title>
+  return (
+    <div className="App">
+      <Title>
+        Styled-components <span>.attrs</span> method
+      </Title>
 
-            <Form>
-                <Field/>
-                <Field/>
-                <Field/>
-            </Form>
-        </div>
-    );
+      <Form>
+        <Field type={"password"} placeholder={"Hell"} title={"..."} />
+        <Field placeholder={"Hi"} />
+        <Field />
+        <Checkbox />
+        <Checkbox />
+        <Checkbox />
+        <Checkbox />
+      </Form>
+    </div>
+  );
 }
 
 export default App;
 
 const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    margin-bottom: 32px;
-    span {
-      color: #e91e63;
-    }
+  font-size: 1.5em;
+  text-align: center;
+  margin-bottom: 32px;
+  span {
+    color: #e91e63;
+  }
 `;
 
 const Form = styled.form`
@@ -32,9 +37,20 @@ const Form = styled.form`
   max-width: 500px;
 `;
 
-const Field = styled.input`
+const Field = styled.input.attrs(({ placeholder, title }) => ({
+  placeholder: placeholder || "React",
+  title: title || "nothing...",
+}))`
   padding: 5px 15px;
   margin: 10px 0;
   width: 100%;
   font-size: 1rem;
+`;
+
+const Checkbox = styled.input.attrs(() => ({
+  type: "checkbox",
+  checked: true,
+}))`
+  display: flex;
+  margin-bottom: 10px;
 `;
